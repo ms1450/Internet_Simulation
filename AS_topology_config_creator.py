5d1bfd091afb57387d099fdacb03360e3575331d
mehulsen <ms1450@rit.edu>
2024-03-22 15:29:18 -0400

    Initial commit

---
+++ AS_topology_config_creator.py
@@ -0,0 +1,138 @@
+import os.path
+import pickle
+
+from AS_topology_generator import AutonomousSystem, InternetExchangePoint
+
+
+# Print AS_config.txt
+def print_AS_config(list_of_ASes, list_of_IXPs):
+    with open('./Configuration/AS_config.txt', 'w', newline='\n') as file:
+        for entry in list_of_ASes:
+            if entry.as_id == 1:
+                string = str(entry.as_id) + "\tAS\tConfig\tl3_routers_krill.txt\tl3_links.txt\tempty.txt\tempty.txt\tempty.txt\n"
+            else:
+                string = str(entry.as_id) + "\tAS\tConfig\tl3_routers.txt\tl3_links.txt\tempty.txt\tempty.txt\tempty.txt\n"
+            file.write(string)
+        for entry in list_of_IXPs:
+            string = str(entry.ixp_id) + "\tIXP\tConfig\tN/A\tN/A\tN/A\tN/A\tN/A\n"
+            file.write(string)
+
+
+# Helper function for aslevel_links.txt
+def get_ixp_connections(AS, IXP):
+    connections = []
+    for link in IXP.ixp_connections:
+        if link != AS:
+            connections.append(str(link.as_id))
+    return ",".join(connections)
+
+
+# Print aslevel_links.txt
+def print_aslevel_links(list_of_ASes):
+    with open('./Configuration/aslevel_links.txt', 'w', newline='\n') as file:
+        completed = []
+        for entry in list_of_ASes:
+            completed.append(entry)
+            for customer in entry.customers:
+                if customer not in completed:
+                    string = str(entry.as_id) + "\tRTRA\tProvider\t" + str(
+                        customer.as_id) + "\tRTRA\tCustomer\t100000\t2.5ms\t179." + str(entry.as_id) + "." + str(
+                        customer.as_id) + ".0/24\n"
+                    file.write(string)
+            for peer in entry.peers:
+                if peer not in completed:
+                    string = str(entry.as_id) + "\tRTRA\tPeer\t" + str(
+                        peer.as_id) + "\tRTRA\tPeer\t100000\t2.5ms\t179." + str(entry.as_id) + "." + str(
+                        peer.as_id) + ".0/24\n"
+                    file.write(string)
+            for ixp in entry.ixps:
+                string = str(entry.as_id) + "\tRTRB\tPeer\t" + str(
+                    ixp.ixp_id) + "\tNone\tPeer\t100000\t2.5ms\t" + get_ixp_connections(entry, ixp) + "\n"
+                file.write(string)
+
+
+# Helper function for aslevel_links_students.txt
+def get_smaller_connection(AS1, AS2):
+    if AS1.as_id < AS2.as_id:
+        return str(AS1.as_id) + "." + str(AS2.as_id) + "."
+    else:
+        return str(AS2.as_id) + "." + str(AS1.as_id) + "."
+
+
+# Print as_level_links_students.txt
+def print_aslevel_links_students(list_of_ASes, list_of_IXPs):
+    with open('./Configuration/aslevel_links_students.txt', 'w', newline='\n') as file:
+        for entry in list_of_ASes:
+            for customer in entry.customers:
+                string = str(entry.as_id) + "\tRTRA\tProvider\t" + str(
+                    customer.as_id) + "\tRTRA\tCustomer\t179." + str(entry.as_id) + "." + str(
+                    customer.as_id) + "." + str(entry.as_id) + "/24\n"
+                file.write(string)
+            for provider in entry.providers:
+                string = str(entry.as_id) + "\tRTRA\tCustomer\t" + str(
+                    provider.as_id) + "\tRTRA\tProvider\t179." + str(provider.as_id) + "." + str(
+                    entry.as_id) + "." + str(entry.as_id) + "/24\n"
+                file.write(string)
+            for peer in entry.peers:
+                string = str(entry.as_id) + "\tRTRA\tPeer\t" + str(
+                    peer.as_id) + "\tRTRA\tPeer\t179." + get_smaller_connection(entry, peer) + str(
+                    entry.as_id) + "/24\n"
+                file.write(string)
+            for ixp in entry.ixps:
+                string = str(entry.as_id) + "\tRTRB\tPeer\t" + str(
+                    ixp.ixp_id) + "\tNone\tPeer\t180." + str(ixp.ixp_id) + ".0." + str(
+                    entry.as_id) + "/24\n"
+                file.write(string)
+
+        for ixp in list_of_IXPs:
+            for connection in ixp.ixp_connections:
+                string = str(ixp.ixp_id) + "\tNone\tPeer\t" + str(
+                    connection.as_id) + "\tRTRB\tPeer\t180." + str(
+                    ixp.ixp_id) + ".0." + str(ixp.ixp_id) + "/24\n"
+                file.write(string)
+
+
+# Print l3_routers.txt
+def print_l3_routers():
+    with open('./Configuration/l3_routers.txt', 'w', newline='\n') as file:
+        file.write("RTRA\tDNS\thost:miniinterneteth/d_host\tvtysh\n")
+        file.write("RTRB\tN/A\thost:miniinterneteth/d_routinator\tvtysh\n")
+
+
+# Print l3_routers_krill.txt
+def print_l3_routers_krill():
+    with open('./Configuration/l3_routers_krill.txt', 'w', newline='\n') as file:
+        file.write("RTRA\tDNS\tkrill:miniinterneteth/d_host\tvtysh\n")
+        file.write("RTRB\tN/A\thost:miniinterneteth/d_routinator\tvtysh\n")
+
+
+# Print l3_links.txt
+def print_l3_links():
+    with open('./Configuration/l3_links.txt', 'w', newline='\n') as file:
+        file.write("RTRA\tRTRB\t100000\t10ms\n")
+
+
+if __name__ == "__main__":
+    as_file_name = '.\Topology\Topology_ASes_50.pkl'
+    ixp_file_name = '.\Topology\Topology_IXPs_50.pkl'
+    ASes = []
+    IXPs = []
+    if os.path.exists(as_file_name) and os.path.exists(ixp_file_name):
+        print("[+]\tLoading AS and IXP files...")
+        with open(as_file_name, 'rb') as as_file:
+            ASes = pickle.load(as_file)
+            print(f'[+]\t\tLoaded {len(ASes)} ASes')
+        with open(ixp_file_name, 'rb') as ixp_file:
+            IXPs = pickle.load(ixp_file)
+            print(f'[+]\t\tLoaded {len(IXPs)} IXPs')
+            print("[+]\tCreating Configuration files...")
+            print_AS_config(ASes, IXPs)
+            print_aslevel_links(ASes)
+            print_aslevel_links_students(ASes, IXPs)
+            print_l3_routers()
+            print_l3_routers_krill()
+            print_l3_links()
+            print("[+]\tCompleted")
+    else:
+        print("[-]\tAS and IXP files not found.")
+
---
+++ AS_topology_metrics.py
@@ -0,0 +1,125 @@
+import csv
+import os
+import re
+
+
+# Get List of IXPs
+def get_ixp_nodes(topology_file):
+    connections = []
+    # Read the edge data
+    with open(topology_file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  # Skip the header
+        for row in csvreader:
+            if row[1] == "IXP":
+                numbers = re.findall(r'\d+', row[0])
+                connections.append(int(numbers[0]) if numbers else None)
+    return connections
+
+
+# Extract Network Data (Subnet, Next_Hop, and Path) from the filepath
+def extract_network_data(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+    raw_data = []
+    for line in lines:
+        if line[0] == '*':
+            values = line[3:].split(' ')
+            cleaned = [value.strip() for value in values if value.strip()]
+            raw_data.append(cleaned)
+
+    current_network = ""  # To keep track of network
+    data = []
+    AS_number = ''
+    for data_value in raw_data:
+        path = []
+        if '/' in data_value[0]:
+            current_network = data_value[0]
+            subnet = data_value[0]
+        else:
+            subnet = current_network
+        if '/' not in data_value[0]:
+            next_hop = data_value[0]
+        else:
+            next_hop = data_value[1]
+        if next_hop != '0.0.0.0':
+            reversed_value = data_value[::-1]
+            start_index = len(data_value) - reversed_value.index('0', reversed_value.index('i') + 1) - 1
+            path = data_value[start_index + 1: len(data_value) - 1]
+        else:
+            AS_number = current_network.split('.', 1)[0]
+        data.append([subnet, next_hop, path])
+    return AS_number, data
+
+
+# Parse Links File Metrics
+def topology_metrics(links_file):
+    connections = []
+    # Read the edge data
+    with open(links_file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  # Skip the header
+        for row in csvreader:
+            pair = tuple(sorted(((row[2]), (row[3]))))
+            numeric_pair = tuple(sorted(
+                (int(''.join(i for i in row[2] if i.isdigit())), int(''.join(i for i in row[3] if i.isdigit())))))
+            connections.append(numeric_pair)
+        return connections
+
+
+# Form connections from network data
+def form_network_connections(AS, network_data):
+    connections = []
+    for values in network_data:
+        if len(values[2]) == 1:
+            pair = tuple(sorted((int(AS), int(values[2][0]))))
+            if pair not in connections and AS != values[2][0]:
+                connections.append(pair)
+        elif len(values[2]) > 1:
+            first_pair = tuple(sorted((int(AS), int(values[2][0]))))
+            if first_pair not in connections and AS != values[2][0]:
+                connections.append(first_pair)
+            for i in range(len(values[2]) - 1):
+                next_pair = tuple(sorted((int(values[2][i]), int(values[2][i + 1]))))
+                if next_pair not in connections and values[2][i] != values[2][i + 1]:
+                    connections.append(next_pair)
+    return connections
+
+
+# Print Topology Metrics
+def get_non_ixp_metrics(connections, IXPs):
+    count = 0
+    for connection in connections:  # connection is a tuple like (1, 2)
+        # Check if neither end of the connection is an IXP
+        if not any(node in IXPs for node in connection):
+            count += 1
+    return count
+
+
+def get_network_metrics(folder_path, ixp_nodes, num_links):
+    files = os.listdir(folder_path)
+    sorted_files = sorted(files, key=lambda x: int(re.search(r'(\d+)', x).group()) if re.search(r'(\d+)', x) else float('inf'))
+    for filename in sorted_files:
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            AS, network_data = extract_network_data(file_path)
+            ipbgp_connections = form_network_connections(AS, network_data)
+            num_non_ixp_connections = get_non_ixp_metrics(ipbgp_connections, ixp_nodes)
+            print(f'[+]\t\t{filename}\t\tAS: {AS}\tNon-IXP Connections: {num_non_ixp_connections}/{len(ipbgp_connections)}(~{int((num_non_ixp_connections/num_non_ixp_links)*100)}%)\tEntries: {len(network_data)}')
+
+
+if __name__ == '__main__':
+    node_files = './Topology/Topology_Nodes_50.csv'
+    link_files = './Topology/Topology_Links_50.csv'
+    #ipbgp_folder = './IP_BGP/Pre-Poisoning/'
+    ipbgp_folder = './IP_BGP/Post-Poisoning/50-2/'
+    print("[+]\tReading topology...")
+    ixp_nodes = get_ixp_nodes(node_files)
+    links = topology_metrics(link_files)
+    num_non_ixp_links = get_non_ixp_metrics(links, ixp_nodes)
+    print(f'[+]\tComplete Topology Metrics\tNon-IXP Connections: {num_non_ixp_links}/{len(links)}')
+    get_network_metrics(ipbgp_folder, ixp_nodes, num_non_ixp_links)
+
+
+
+
