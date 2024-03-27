32706ca372e8f99a0f292e0ff68a60d21a6cf112
mehulsen <ms1450@rit.edu>
2024-03-27 13:15:22 -0400

    Improved the clean_and_extract_ipbgp.sh script, cleaned up folders and removed junk scripts

--- clean_and_extract_ipbgp.sh
+++ clean_and_extract_ipbgp.sh
@@ -1,46 +1,56 @@
 #!/bin/bash
 
+echo "Configuration Cleaner and Extractor"
 # Ask for a number and read it into a variable
-echo -n "Enter AS: "
+echo -n "[i] Enter AS: "
 read num_zip
+base_dir=/home/student/Desktop/Simulation/
+ipbgp_dir="${base_dir}/IP_BGP_Path/"
 
 # Global variable for the number of Autonomous Systems, set to 50 by default
 num=50
 
 # Create the directory if it does not exist
-mkdir -p /home/student/Desktop/IPBGP/
+mkdir -p "${ipbgp_dir}"
 
 # First loop: Clear IP BGP sessions for each router
+echo "[+] Cleaning IP BGP sessions for each AS"
 for ((current=1; current<=num; current++))
 do
+    echo "[+] Cleaning ${current}.."
     # Execute the command in the docker container for each router
     docker exec "${current}_RTRArouter" vtysh -c 'clear ip bgp *'
 done
+echo "[+] Cleaning Completed"
 
 # Second loop: Check the size of looking_glass.txt files and move them if conditions are met
+echo "[+] Moving IP BGP Paths"
 for ((current=1; current<=num; current++))
 do
-    src_file="/home/student/Desktop/Simulation/platform/groups/g${current}/RTRA/looking_glass.txt"
-    dest_file="/home/student/Desktop/IPBGP/${current}.txt"
+    src_file="${base_dir}/platform/groups/g${current}/RTRA/looking_glass.txt"
+    dest_file="${ipbgp_dir}/${current}.txt"
 
     while true; do
         if [[ -f "$src_file" && $(stat --printf="%s" "$src_file") -ge 4000 ]]; then
             mv "$src_file" "$dest_file"
-            echo "Moved $src_file to $dest_file"
+            echo "[+] Moved $src_file to $dest_file"
             break
         else
-            echo "File $src_file is smaller than 4000 bytes. Waiting..."
+            echo "[-] File $src_file is smaller than 4000 bytes. Waiting..."
             sleep 3
         fi
     done
 done
 
-echo "All files processed. Zipping..."
+echo "[+] Completed Moving files processed. Zipping..."
 
 # Go to the IPBGP directory
-cd /home/student/Desktop/IPBGP/
+cd "${ipbgp_dir}"
 
 # Create a zip archive with all the .txt files
 zip "${num_zip}.zip" *.txt
+echo "[+] Completed Packaging ${num_zip}.zip"
 
-echo "Completed Packaging into ${num_zip}.zip"
+# Cleaning temporary txt files
+rm -f *.txt
+echo "[+] Completed, Goodbye!"
